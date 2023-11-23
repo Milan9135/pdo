252494d5f0dd92d61a79bf070eb74ce41584324a
Milan9135 <Milpower0@gmail.com>
2023-11-23 15:42:06 +0100

    Initial commit

---
+++ home.php
@@ -0,0 +1,19 @@
+<?php 
+
+include "db.php";
+
+$db = new Database();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+<body>
+    <p>text</p>
+</body>
+</html>
