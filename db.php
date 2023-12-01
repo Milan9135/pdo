48704233d8d00e9bdacdd4e8f217030dd87f857d
Milan9135 <Milpower0@gmail.com>
2023-12-01 11:18:42 +0100

    select

--- db.php
+++ db.php
@@ -17,8 +17,20 @@ class Database {
         $sql = "INSERT INTO users (name, email)
         VALUES (?, ?)";
         $statement = $this->pdo->prepare($sql);
-
+        
         $statement->execute([$name, $email]);
+    }
+
+    public function select($id = null) {
+        if ($id != null) {
+            $stmt = $this->pdo->prepare("SELECT * FROM users WHERE id = ?");
+            $stmt->execute([$id]);
+            $result = $stmt->fetch();
+        } else {
+            $stmt = $this->pdo->query("SELECT * FROM users");
+            $result = $stmt->fetchAll();
+        }
+        return ($result);
     }
 }
 
--- home.php
+++ home.php
@@ -8,6 +8,9 @@ if (isset($_POST['knopje'])) {
     $db->insert($_POST['name'], $_POST['email']);
 }
 
+$data = $db->select(2);
+
+print_r($data);
 ?>
 
 <!DOCTYPE html>
@@ -16,6 +19,11 @@ if (isset($_POST['knopje'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test</title>
+    <style>
+        a {
+            margin: 3px;
+        }
+    </style>
 </head>
 <body>
     <form method="POST">
@@ -23,5 +31,31 @@ if (isset($_POST['knopje'])) {
         <input type="email" name="email">
         <input type="submit" name="knopje">
     </form>
+    <table border="1">
+        <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>email</th>
+            <th>actie</th>
+        </tr>
+<!--                met fetchall
+
+            <?php // foreach($data as $user) { ?>
+            <tr>
+                <td><?php echo $user['id']?></td>
+                <td><?php echo $user['name']?></td>
+                <td><?php echo $user['email']?></td>
+                <td><a href="">edit</a><a href="">delete</a></td>
+            </tr>
+            <?php // } ?>      
+-->
+                <!-- met fetch (1 rij)
+            <tr>
+                <td><?php echo $data['id']?></td>
+                <td><?php echo $data['name']?></td>
+                <td><?php echo $data['email']?></td>
+                <td><a href="">edit</a><a href="">delete</a></td>
+            </tr>  
+    </table>
 </body>
 </html>
